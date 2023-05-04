4f81b24efe6e602727e1146d306c82213398c05b
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2023-05-04 18:31:51 +0200

    updated Potentiometer

--- Potentiometer/Potentiometer.cpp
+++ Potentiometer/Potentiometer.cpp
@@ -1,1 +1,40 @@
+#include <Potentiometer.h>
 
+Hardware::Potentiometer::Potentiometer(uint8_t pin, Direction direction)
+{
+    _pin = pin;
+    _direction = direction;
+}
+
+uint16_t Hardware::Potentiometer::get()
+{
+    _lastValue = analogRead(_pin);
+
+    if (_direction == UP)
+        return analogRead(_pin);
+
+    if (_direction == DOWN)
+        return 1023 - analogRead(_pin);
+}
+
+uint8_t Hardware::Potentiometer::getPercentage()
+{
+    _lastValue = analogRead(_pin);
+
+    if (_direction == UP)
+        return map(analogRead(_pin), 0, 1023, 0, 100);
+
+    if (_direction == DOWN)
+        return map(1023 - analogRead(_pin), 0, 1023, 0, 100);
+}
+
+bool Hardware::Potentiometer::hasChanged()
+{
+    if (_lastValue != analogRead(_pin))
+    {
+        _lastValue = analogRead(_pin);
+        return 1;
+    }
+
+    return 0;
+}
--- Potentiometer/Potentiometer.h
+++ Potentiometer/Potentiometer.h
@@ -1,1 +1,24 @@
+#include <Arduino.h>
 
+namespace Hardware
+{
+    class Potentiometer
+    {
+    private:
+        enum Direction
+        {
+            UP,
+            DOWN
+        };
+
+        uint8_t _pin;
+        uint16_t _lastValue;
+        Direction _direction;
+
+    public:
+        Potentiometer(uint8_t pin, Direction direction);
+        uint16_t get();
+        uint8_t getPercentage();
+        bool hasChanged();
+    };
+} // namespace Hardware
