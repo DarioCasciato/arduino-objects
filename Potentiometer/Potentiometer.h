9435bff4edf4360fd6fcbc063218895e46c5c080
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2023-05-04 21:54:58 +0200

    moved enum outside of class

--- Potentiometer/Potentiometer.h
+++ Potentiometer/Potentiometer.h
@@ -4,13 +4,13 @@ namespace Hardware
 {
     class Potentiometer
     {
-    private:
         enum Direction
         {
             UP,
             DOWN
         };
 
+    private:
         uint8_t _pin;
         uint16_t _lastValue;
         Direction _direction;
